1e0eb97c8102a66e2ec9310dbc1637c7f800c370
Михаил Королев <michael199-fts@yandex.ru>
2021-06-17 12:49:50 +0300

    Изменения в моделях оценок постов и комментариев

--- photoload/admin.py
+++ photoload/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from photoload.models import User
 from photoload.models import Post
-from photoload.models import Value
+from photoload.models import Value_post
 from photoload.models import Comment
+from photoload.models import Value_comm
 admin.site.register(User)
 admin.site.register(Post)
-admin.site.register(Value)
+admin.site.register(Value_post)
 admin.site.register(Comment)
+admin.site.register(Value_comm)
--- photoload/models.py
+++ photoload/models.py
@@ -2,29 +2,34 @@ from django.db import models
 import datetime
 
 class User(models.Model):
-    name = models.CharField(max_length=40)
-    email = models.EmailField(blank=True)
-    password = models.CharField(max_length=50)
-    photo = models.ImageField(blank=True)
-    id_user = models.AutoField(primary_key = True, unique=True)
+    name = models.CharField(max_length=40, verbose_name="Имя пользователя")
+    email = models.EmailField(blank=True, verbose_name="E-mail пользователя")
+    password = models.CharField(max_length=50, verbose_name="Пароль")
+    photo = models.ImageField(blank=True, verbose_name="Аватар")
+    id_user = models.AutoField(primary_key = True, unique=True, verbose_name="ID пользователя")
 
 class Post(models.Model):
-    photo = models.ImageField()
-    name = models.CharField(max_length=30)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    raters = models.IntegerField(default=0)
-    sum_rating = models.IntegerField(default=0)
-    upload_date = models.DateTimeField(default=datetime.datetime.now())
+    photo = models.ImageField(verbose_name="Фото поста")
+    raters = models.SmallIntegerField(default=0, verbose_name="Число оценщиков")
+    sum_rate = models.SmallIntegerField(default=0, verbose_name="Сумма оценок")
+    name = models.CharField(max_length=30, verbose_name="Название")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор фото")
+    upload_date = models.DateTimeField(verbose_name="Время загрузки")
 
 class Comment(models.Model):
-    text = models.TextField(max_length=300)
-    sum_rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    target = models.ForeignKey(Post, on_delete=models.CASCADE)
-    upload_date = models.DateTimeField(default=datetime.datetime.now())
+    text = models.TextField(max_length=300, verbose_name="Текст комментария")
+    raters = models.SmallIntegerField(default=0, verbose_name="Число оценщиков")
+    sum_rate = models.SmallIntegerField(default=0, verbose_name="Сумма оценок")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор комментария")
+    target = models.ForeignKey(Post, on_delete=models.CASCADE,verbose_name="Пост")
+    upload_date = models.DateTimeField(verbose_name="Время написания")
 
-class Value(models.Model):
-    rate = models.IntegerField(default=1)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    target_post = models.ForeignKey(Post, on_delete=models.CASCADE, blank=True)
-    target_com = models.ForeignKey(Comment, on_delete=models.CASCADE, blank=True)
+class Value_post(models.Model):
+    rate = models.SmallIntegerField(verbose_name="Оценка")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор оценки")
+    target = models.ForeignKey(Post, on_delete=models.CASCADE,verbose_name="Пост")
+
+class Value_comm(models.Model):
+    rate = models.SmallIntegerField(verbose_name="Оценка")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор оценки")
+    target = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name="Комментарий")
