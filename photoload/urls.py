9f2b61eafb825363c36ef879bcaa5fe8a13f22f9
Михаил Королев <michael199-fts@yandex.ru>
2021-06-19 15:11:37 +0300

    Добавлено добавление постов

--- photoload/urls.py
+++ photoload/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from photoload.views import PostList
+from photoload.views import PostList, PostCreateList
 
 urlpatterns = [
-    path('posts/', PostList.as_view())
+    path('posts/', PostList.as_view()),
+    path('create/', PostCreateList.as_view()),
 ]
--- photoload/views.py
+++ photoload/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, ListCreateAPIView
 from photoload.serializers import PostSerializer
 from rest_framework.response import Response
 from photoload.models import Post
@@ -8,6 +8,15 @@ class PostList(ListAPIView):
     serializer_class = PostSerializer
 
     def get(self, request):
+        queryset = self.get_queryset()
+        serializer = PostSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+class PostCreateList(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def list(self, request):
         queryset = self.get_queryset()
         serializer = PostSerializer(queryset, many=True)
         return Response(serializer.data)
