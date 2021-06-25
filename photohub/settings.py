7255151e0050654a95cab129aff1475800d4573e
Михаил Королев <michael199-fts@yandex.ru>
2021-06-26 00:04:24 +0300

    Смена базы данных на PostgreSQL

--- photohub/settings.py
+++ photohub/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'photohub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'photoload',
+    'USER' : 'postgres',
+    'PASSWORD' : 'zip234yui',
+    'HOST' : '127.0.0.1',
+    'PORT' : '5432',
     }
 }
 
