7d5207f0fef78fd2e735f84684df666c0a4d73cd
Михаил Королев <michael199-fts@yandex.ru>
2021-06-29 00:19:28 +0300

    fix

--- photoload/urls.py
+++ photoload/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('update/post/', PostUpdate.as_view()),
     path('delete/post/', PostDelete.as_view()),
     path('reg/', UserReg.as_view()),
-    path('/', UserList.as_view()),
+    path('home/', UserList.as_view()),
     path('settings/', UserChange.as_view()),
     path('del_user', UserDelete.as_view()),
 ]
