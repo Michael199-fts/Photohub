140de9f7a647ef4df3ebf5402fe5e8f216711baa
Михаил Королев <michael199-fts@yandex.ru>
2021-07-11 17:08:59 +0300

    Изменение моделей

--- photoload/models.py
+++ photoload/models.py
@@ -10,8 +10,6 @@ class User(AbstractUser):
 
 class Post(models.Model):
     photo = models.ImageField(verbose_name="Фото поста")
-    raters = models.SmallIntegerField(default=0, verbose_name="Число оценщиков")
-    sum_rate = models.SmallIntegerField(default=0, verbose_name="Сумма оценок")
     name = models.CharField(max_length=30, verbose_name="Название")
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Автор фото")
     upload_date = models.DateTimeField(auto_now_add=True, verbose_name="Время загрузки")
@@ -19,20 +17,14 @@ class Post(models.Model):
 
 class Comment(models.Model):
     text = models.TextField(max_length=300, verbose_name="Текст комментария")
-    raters = models.SmallIntegerField(default=0, verbose_name="Число оценщиков")
-    sum_rate = models.SmallIntegerField(default=0, verbose_name="Сумма оценок")
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Автор комментария")
     target = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name="Пост")
+    target_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True,
+                                       verbose_name="Комментарий")
     upload_date = models.DateTimeField(auto_now_add=True, verbose_name="Время написания")
 
 
-class Value_post(models.Model):
+class Rate(models.Model):
     rate = models.SmallIntegerField(verbose_name="Оценка")
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Автор оценки")
     target = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name="Пост")
-
-
-class Value_comm(models.Model):
-    rate = models.SmallIntegerField(verbose_name="Оценка")
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Автор оценки")
-    target = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name="Комментарий")
--- photoload/serializers.py
+++ photoload/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from photoload.models import User, Value_post, Value_comm, Comment, Post
+from photoload.models import User, Comment, Post
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,22 +11,10 @@ class UserSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ["raters", "sum_rate", "name", "author", "photo"]
-
-
-class ValuePostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Value_post
-        fields = ["rate", "author", "target"]
-
-
-class ValueCommSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Value_comm
-        fields = ["rate", "author", "target"]
+        fields = ["name", "author", "photo"]
 
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ["text", "raters", "sum_rate", "target", "author"]
+        fields = ["text", "target", "target_comment", "author"]
