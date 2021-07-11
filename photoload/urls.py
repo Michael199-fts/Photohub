eda7b538e157afa77b9f1eadd2438b16abec4b53
Михаил Королев <michael199-fts@yandex.ru>
2021-07-11 14:19:34 +0300

    Переопределено представление поста

--- photoload/urls.py
+++ photoload/urls.py
@@ -1,13 +1,6 @@
 from django.urls import path
-from photoload.views import PostGet, PostCreate, PostUpdate, PostDelete, UserReg, UserList, UserChange, UserDelete
+from photoload.views import PostCreateUpdateGet
 
 urlpatterns = [
-    path('get/post/', PostGet.as_view()),
-    path('create/post/', PostCreate.as_view()),
-    path('update/post/', PostUpdate.as_view()),
-    path('delete/post/', PostDelete.as_view()),
-    path('reg/', UserReg.as_view()),
-    path('home/', UserList.as_view()),
-    path('settings/', UserChange.as_view()),
-    path('del_user', UserDelete.as_view()),
+    path('post/', PostCreateUpdateGet.as_view()),
 ]
