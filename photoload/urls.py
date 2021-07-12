311258a4993a7a26dff62170f4eaa7e33f606d46
Михаил Королев <michael199-fts@yandex.ru>
2021-07-12 20:41:45 +0300

    Изменение моделей. Добавлено представление регистрации и аутентификации

--- photoload/urls.py
+++ photoload/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from photoload.views import PostCreateUpdateGet
+from photoload.views import PostCreateUpdateGet, LoginAPIView, RegistrationAPIView
 
 urlpatterns = [
     path('post/', PostCreateUpdateGet.as_view()),
+    path('login/', LoginAPIView.as_view()),
+    path('auth/', RegistrationAPIView.as_view()),
 ]
--- photoload/views.py
+++ photoload/views.py
@@ -1,6 +1,9 @@
 from rest_framework import status
 from rest_framework.generics import ListAPIView
-from photoload.serializers import PostSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+
+from photoload.serializers import PostSerializer, RegistrationSerializer, LoginSerializer
 from rest_framework.response import Response
 from photoload.models import Post
 
@@ -14,10 +17,11 @@ class PostCreateUpdateGet(ListAPIView):
         serializer = PostSerializer(queryset, many=True)
         return Response(serializer.data)
 
-
     def post(self, request):
         queryset = self.get_queryset()
-        serializer = PostSerializer(queryset, many=True)
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data)
 
     def update(self, request):
@@ -26,8 +30,28 @@ class PostCreateUpdateGet(ListAPIView):
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
 
-
     def delete(self, request, *args, **kwargs):
         instance = self.get_object()
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class RegistrationAPIView(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = RegistrationSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'token': serializer.data.get('token', None),},status=status.HTTP_201_CREATED,)
+
+
+class LoginAPIView(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
