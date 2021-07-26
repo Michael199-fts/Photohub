a11cbdbc41d78b0b43e8233113f35dd76d1efadf
Михаил Королев <michael199-fts@yandex.ru>
2021-07-26 14:39:04 +0300

    Добавлены представления комментариев. Снос БД

--- photoload/urls.py
+++ photoload/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from photoload.views import PostUpdateDelete, RegistrationAPIView, AuthTokenView, PersonalAccountView,\
-    PostListView, PostCreateView
+from photoload.views import PostUpdateDeleteView, RegistrationAPIView, AuthTokenView, PersonalAccountView, \
+    PostListView, PostCreateView, CommentListView, NestedCommentView, CreateCommentView, UpdateDeleteCommentView
 
 urlpatterns = [
-    path('post/<int:pk>', PostUpdateDelete.as_view(), name='Редактор постов'),
+    path('post/<int:pk>', PostUpdateDeleteView.as_view(), name='Редактор постов'),
     path('c_post/', PostCreateView.as_view(), name='Создание поста'),
     path('reg/', RegistrationAPIView.as_view(), name='Регистрация'),
     path('auth/', AuthTokenView.as_view(), name='Аутентификация'),
     path('pa/<int:pk>', PersonalAccountView.as_view(), name='Личный кабинет'),
     path('posts/', PostListView.as_view(), name='Лист постов'),
+    path('comments/<int:pk>', CommentListView.as_view(), name='Комментарии к посту'),
+    path('nested_comments/<int:pk>', NestedCommentView.as_view(), name='Вложенные комментарии'),
+    path('c_comment/', CreateCommentView.as_view(), name='Создание комментария'),
+    path('comment/<int:pk>', UpdateDeleteCommentView.as_view(), name='Редактор комментария'),
 ]
--- photoload/views.py
+++ photoload/views.py
@@ -1,14 +1,15 @@
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.generics import UpdateAPIView, CreateAPIView
+from rest_framework.generics import UpdateAPIView, CreateAPIView, ListAPIView
+from rest_framework.pagination import CursorPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
 
 from photoload.paginator import CustomPagination
-from photoload.serializers import PostSerializer, RegistrationSerializer, PersonalAccountSerializer
+from photoload.serializers import PostSerializer, RegistrationSerializer, PersonalAccountSerializer, CommentSerializer
 from rest_framework.response import Response
-from photoload.models import Post, User
+from photoload.models import Post, User, Comment
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -21,9 +22,12 @@ class PersonalAccountView(UpdateAPIView):
     queryset = User.objects.all()
 
     def get(self, request, pk):
-        user_fields = User.objects.get(id_user=pk)
-        serializer = self.serializer_class(user_fields)
-        return Response(serializer.data)
+        try:
+            user_fields = User.objects.get(id_user=pk)
+            serializer = self.serializer_class(user_fields)
+            return Response(serializer.data)
+        except ObjectDoesNotExist:
+            return Response("Пользователь не найден", status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         try:
@@ -68,6 +72,7 @@ class PostListView(APIView):
 class PostCreateView(CreateAPIView):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
+    queryset = Post.objects.all()
 
     def get(self, request):
         posts = Post.objects.all()
@@ -75,15 +80,18 @@ class PostCreateView(CreateAPIView):
         return Response(serializer.data)
 
 
-class PostUpdateDelete(UpdateAPIView):
+class PostUpdateDeleteView(UpdateAPIView):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
     queryset = Post.objects.all()
 
     def get(self, request, pk):
-        post = Post.objects.get(id=pk)
-        serializer = self.serializer_class(post)
-        return Response(serializer.data)
+        try:
+            post = Post.objects.get(id=pk)
+            serializer = self.serializer_class(post)
+            return Response(serializer.data)
+        except ObjectDoesNotExist:
+            return Response("Пост не найден", status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         try:
@@ -94,7 +102,60 @@ class PostUpdateDelete(UpdateAPIView):
             return Response("Пост не найден", status=status.HTTP_400_BAD_REQUEST)
 
 
+class CommentListView(ListAPIView):
+    serializer_class = CommentSerializer
+    pagination_class = CursorPagination
+    queryset = Comment.objects.all()
 
+    def get(self, request, pk):
+        try:
+            posts = Comment.objects.filter(target=pk)
+            serializer = self.serializer_class(posts, many=True)
+            return Response(serializer.data)
+        except ObjectDoesNotExist:
+            return Response("Комментарий не найден", status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class NestedCommentView(ListAPIView):
+    serializer_class = CommentSerializer
+    pagination_class = CursorPagination
+    queryset = Comment.objects.all()
+
+    def get(self, request, pk):
+        try:
+            posts = Comment.objects.filter(target_comment=pk)
+            serializer = self.serializer_class(posts, many=True)
+            return Response(serializer.data)
+        except ObjectDoesNotExist:
+            return Response("Комментарий не найден", status=status.HTTP_400_BAD_REQUEST)
+
+
+class CreateCommentView(CreateAPIView):
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
+    permission_classes = [IsAuthenticated]
+
+class UpdateDeleteCommentView(UpdateAPIView):
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+        try:
+            comment = Comment.objects.get(id=pk)
+            serializer = self.serializer_class(comment)
+            return Response(serializer.data)
+        except ObjectDoesNotExist:
+            return Response("Комментарий не найден", status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        try:
+            instance = Comment.objects.get(id=pk)
+            instance.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except ObjectDoesNotExist:
+            return Response("Комментарий не найден", status=status.HTTP_400_BAD_REQUEST)
 #{
 #"username": "",
 #"password": ""
